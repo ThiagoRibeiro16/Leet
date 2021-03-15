2a08ae511a1d513231c5136ddceb883fba527e53
Thiago Ribeiro <thiago.escola321@gmail.com>
2021-03-15 00:30:53 -0300

    Finalização do Programa

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Leet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         string tradução;
+         
+         Console.Write("Digite uma frase:");
+         tradução =Console.ReadLine();
+         Console.WriteLine();
+         Console.Write($"Sua frase traduzida em Leet:{tradução.Replace ("a" , "4").Replace("e" , "3").Replace("i" , "1").Replace("o" , "0").Replace ("t" , "7").Replace("s" , "5") }");
+
+
+
+            
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # Leet
 Faça um tradutor simples para a linguagem Leet Speak.
+
+A ou a	4
+E ou e	3
+I ou i ou L ou l	1
+O ou o	0
+T ou t	7
+S ou s	5
